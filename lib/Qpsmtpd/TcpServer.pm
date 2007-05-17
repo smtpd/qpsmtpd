ccf990e03290bdf51355eaac32db6ea5731b9e4e
Matt Sergeant <matt@sergeant.org>
2007-05-17 22:16:27 +0000

    IPv6 support from issue #7.
    git-svn-id: https://svn.perl.org/qpsmtpd/trunk@740 958fd67b-6ff1-0310-b445-bb7760255be9

--- lib/Qpsmtpd/TcpServer.pm
+++ lib/Qpsmtpd/TcpServer.pm
@@ -8,6 +8,23 @@ use strict;
 
 use POSIX ();
 
+my $has_ipv6;
+if (
+    eval {require Socket6;} &&
+    # INET6 prior to 2.01 will not work; sorry.
+    eval {require IO::Socket::INET6; IO::Socket::INET6->VERSION("2.00");}
+    ) {
+    import Socket6;
+    $has_ipv6=1;
+}
+else {
+    $has_ipv6=0;
+}
+
+sub has_ipv6 {
+    return $has_ipv6;
+}
+
 my $first_0; 
 
 sub start_connection {
@@ -102,6 +119,45 @@ sub disconnect {
   $self->SUPER::disconnect(@_);
   $self->run_hooks("post-connection");
   exit;
+}
+
+# local/remote port and ip address
+sub lrpip {
+  my ($server, $client, $hisaddr) = @_;
+
+  my ($port, $iaddr) = ($server->sockdomain == AF_INET) ? (sockaddr_in($hisaddr)) : (sockaddr_in6($hisaddr));
+  my $localsockaddr = getsockname($client);
+  my ($lport, $laddr) = ($server->sockdomain == AF_INET) ? (sockaddr_in($localsockaddr)) : (sockaddr_in6($localsockaddr));
+
+  my $nto_iaddr = ($server->sockdomain == AF_INET) ? (inet_ntoa($iaddr)) : (inet_ntop(AF_INET6(), $iaddr));
+  my $nto_laddr = ($server->sockdomain == AF_INET) ? (inet_ntoa($laddr)) : (inet_ntop(AF_INET6(), $laddr));
+  $nto_iaddr =~ s/::ffff://;
+  $nto_laddr =~ s/::ffff://;
+
+  return ($port, $iaddr, $lport, $laddr, $nto_iaddr, $nto_laddr);
+}
+
+sub tcpenv {
+  my ($nto_laddr, $nto_iaddr, $no_rdns) = @_;
+
+  my $TCPLOCALIP  = $nto_laddr;
+  my $TCPREMOTEIP = $nto_iaddr;
+  
+  if ($no_rdns) {
+    return ($TCPLOCALIP, $TCPREMOTEIP, $TCPREMOTEIP ? "[$ENV{TCPREMOTEIP}]" : "[noip!]");
+  }
+  my $res = new Net::DNS::Resolver;
+  $res->tcp_timeout(3);
+  $res->udp_timeout(3);
+  my $query = $res->query($nto_iaddr);
+  my $TCPREMOTEHOST;
+  if($query) {
+    foreach my $rr ($query->answer) {
+      next unless $rr->type eq "PTR";
+      $TCPREMOTEHOST = $rr->ptrdname;
+    }
+  }
+  return ($TCPLOCALIP, $TCPREMOTEIP, $TCPREMOTEHOST || "Unknown");
 }
 
 1;
