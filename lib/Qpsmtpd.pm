2ef46f46946823391754eb77b5fe6dcfd16f9edb
Ask Bjørn Hansen <ask@develooper.com>
2007-06-11 18:03:14 +0000

    change version numbers to 0.40
    git-svn-id: https://svn.perl.org/qpsmtpd/trunk@754 958fd67b-6ff1-0310-b445-bb7760255be9

--- lib/Qpsmtpd.pm
+++ lib/Qpsmtpd.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION $Logger $TraceLevel $Spool_dir $Size_threshold);
 use Sys::Hostname;
 use Qpsmtpd::Constants;
 
-$VERSION = "0.40_02";
+$VERSION = "0.40";
 
 sub version { $VERSION };
 
