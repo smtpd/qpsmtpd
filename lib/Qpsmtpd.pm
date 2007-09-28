1d4eca3ab3befdf1ff6c116b821035a4578fb936
Ask Bjørn Hansen <ask@develooper.com>
2007-09-28 07:45:11 +0000

    update changes and manifest
    bump version to 0.42rc1
    
    
    git-svn-id: https://svn.perl.org/qpsmtpd/trunk@803 958fd67b-6ff1-0310-b445-bb7760255be9

--- lib/Qpsmtpd.pm
+++ lib/Qpsmtpd.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION $Logger $TraceLevel $Spool_dir $Size_threshold);
 use Sys::Hostname;
 use Qpsmtpd::Constants;
 
-$VERSION = "0.41";
+$VERSION = "0.42rc1";
 
 sub version { $VERSION };
 
