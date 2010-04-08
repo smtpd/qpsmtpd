c408aa98cae9e1e07a40c3cc4e0f87c11451024a
Ask Bjørn Hansen <ask@develooper.com>
2010-04-07 22:30:30 -0700

    Update copyright year

--- lib/Qpsmtpd.pm
+++ lib/Qpsmtpd.pm
@@ -625,7 +625,7 @@ L<http://smtpd.develooper.com/> and the I<README> file for more information.
 
 =head1 COPYRIGHT
 
-Copyright 2001-2009 Ask Bjørn Hansen, Develooper LLC.  See the
+Copyright 2001-2010 Ask Bjørn Hansen, Develooper LLC.  See the
 LICENSE file for more information.
 
 
