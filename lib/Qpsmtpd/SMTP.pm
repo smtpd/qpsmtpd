b22d037c393be81d1208a74872874010ecf1119e
Jared Johnson <jjohnson@efolder.net>
2014-09-09 18:08:09 -0500

    Add hook_user_config
    This adds a hook_user_config for plugins designed to return per-user
    configuration directives.  Qpsmtpd::Address::config() triggers a
    hook_user_config plugin.

--- lib/Qpsmtpd/SMTP.pm
+++ lib/Qpsmtpd/SMTP.pm
@@ -396,6 +396,7 @@ sub mail_pre_respond {
     }
     return $self->respond(501, "could not parse your mail from command")
       unless $from;
+    $from->notes('qp_obj',$self);
 
     $self->run_hooks("mail", $from, %$param);
 }
@@ -485,6 +486,7 @@ sub rcpt_pre_respond {
     return $self->respond(501, "could not parse recipient")
       if (!$rcpt or ($rcpt->format eq '<>'));
 
+    $rcpt->notes('qp_obj',$self);
     $self->run_hooks("rcpt", $rcpt, %$param);
 }
 
