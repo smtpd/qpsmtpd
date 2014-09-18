c079281b4d30e8d08088a288793cfac093b9935b
Jonathan Hall <jhall@efolder.net>
2014-09-17 19:23:50 -0500

    Fix remaining tests.  - Fixes cache entry for 'perl.test'  - Fixes CNAME recursion in FakeResolver  - Handles IP reversing better

--- t/Test/Qpsmtpd.pm
+++ t/Test/Qpsmtpd.pm
@@ -10,7 +10,7 @@ use parent 'Qpsmtpd::SMTP';
 
 use Qpsmtpd::Constants;
 use Test::Qpsmtpd::Plugin;
-use Test::FakeResolver './t/dns_cache.txt';
+use Test::FakeResolver;
 
 sub new_conn {
     ok(my $smtpd = __PACKAGE__->new(), "new");
@@ -120,7 +120,9 @@ sub run_plugin_tests {
 
 sub dns_resolver {
     my $self = shift;
-    return Test::FakeResolver->new(@_);
+    return Test::FakeResolver->new(@_,
+        static_file => './t/dns_cache.txt'
+    );
 }
 
 1;
