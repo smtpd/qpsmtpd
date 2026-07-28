77e9b4fe9d6b22e10ac07c2af1c9d8ec8fb40593
Todd Zullinger <tmz@pobox.com>
2026-07-28 10:33:01 -0400

    postfix: disable $qid debug output (#345)

--- lib/Qpsmtpd/Postfix.pm
+++ lib/Qpsmtpd/Postfix.pm
@@ -188,7 +188,7 @@ sub inject_mail {
 
     my %at  = $strm->get_attr;
     my $qid = $at{queue_id};
-    print STDERR "qid=$qid\n";
+    #print STDERR "qid=$qid\n";
     $strm->print_attr('flags' => $transaction->notes('postfix-queue-flags'));
     $strm->print_rec_time();
     $strm->print_rec('REC_TYPE_FROM', $transaction->sender->address || "");
